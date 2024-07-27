8b31a1c1729e765012b85073392a9482e4984e8e
Nurlan199206 <user_1771@mail.ru>
2024-07-27 14:58:43 +0500

    add new servers

--- provider.tf
+++ provider.tf
@@ -11,5 +11,5 @@ provider "proxmox" {
     pm_tls_insecure = true
     pm_api_url = "https://192.168.1.75:8006/api2/json"
     pm_api_token_id = "root@pam!terraform"
-    pm_api_token_secret = "6d0124fd-1528-42b2-836c-ccfd56e72169"
+    pm_api_token_secret = "cdd3f2b4-124e-4660-8c80-5b8defd5a7d6"
 }
